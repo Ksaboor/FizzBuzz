20448fc682d681ab558d104124c6cac5266a0d79
Khail Saboor <khalilsaboor1@gmail.com>
2018-09-27 13:23:44 -0400

    FizzBuzz in C#

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         //   Console.WriteLine("Hello World!");
+            FizzBuzzprinter(50);
+        }
+
+        private static void FizzBuzzprinter(int i)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                if (i % 15 == 0)
+                
+                    Console.WriteLine("FizzBuzz" + " ");
+                    
+                else if(i % 3 == 0)
+                
+                    Console.WriteLine("Fizz" + " ");
+                
+                else if(i % 5 == 0)
+                
+                    Console.WriteLine("Buzz" + " ");
+                
+                else
+                
+                    Console.WriteLine(i + " ");
+                
+            }
+        }
+    }
+}
