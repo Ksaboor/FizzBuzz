d615096d837037c5d009376655d22394eeaf9134
Khail Saboor <khalilsaboor1@gmail.com>
2018-09-28 17:25:39 -0400

    fix($Program.cs): fix Buzz error
    replace the number being divide to j instead of i
    
    BREAKING CHANGE: FizzBuzz method working properly now

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -7,27 +7,29 @@ namespace FizzBuzz
         static void Main(string[] args)
         {
             //   Console.WriteLine("Hello World!");
-            FizzBuzzprinter(50);
+            FizzBuzzprinter(100);
         }
 
-        private static void FizzBuzzprinter(int i)
+        public static void FizzBuzzprinter(int i)
         {
-            var str = "";
-            for (var j = 1; j <= i; j++)
+            for (int j = 1; j <= i; j++)
             {
-                if (i % 3 == 0)  
-                {  
-                    str += "Fizz";  
-                }  
-                if (i % 5 == 0)  
-                {  
-                    str += "Buzz";  
-                }  
-                if (str.Length == 0)  
-                {  
-                    str = i.ToString();  
-                }  
-                Console.WriteLine(str); 
+                if (j % 3 == 0 && j % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz"); 
+                }
+                else if (j % 3 == 0 && j % 5 != 0)
+                {
+                 
+                    Console.WriteLine("Fizz");
+                } else if (j % 3 != 0 && j % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
             }
         }
     }
